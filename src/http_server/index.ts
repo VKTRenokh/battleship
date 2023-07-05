c03f555bd0920e53d73abe742ca83ac69cf17835
VKTRenokh <viktorenokh@gmail.com>
2023-07-05 07:41:26 +0300

    feat: add ws

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -1,18 +1,34 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as http from "http";
+import * as ws from "ws";
 
 export const httpServer = http.createServer(function (req, res) {
   const __dirname = path.resolve(path.dirname(""));
-  const file_path =
+  const filePath =
     __dirname + (req.url === "/" ? "/front/index.html" : "/front" + req.url);
-  fs.readFile(file_path, function (err, data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
+
+  const readable = fs.createReadStream(filePath);
+
+  readable.on("error", (error) => {
+    res.writeHead(404);
+    res.end(JSON.stringify(error));
+  });
+
+  readable.pipe(res);
+});
+
+const webSocketServer = new ws.WebSocketServer({
+  host: "localhost",
+  port: 3000,
+});
+
+const connections: ws.ClientOptions[] = [];
+
+webSocketServer.on("connection", (socket, req) => {
+  connections.push(socket);
+
+  socket.on("message", (data, isBinary) => {
+    console.log(data.toString());
   });
 });
